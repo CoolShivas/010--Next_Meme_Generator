c5e347fc4a8a65166705c94cf3849c4692253e1c
CoolShivas <shivas2710cool00@gmail.com>
2025-09-06 20:39:28 +0530

    Converting the js files to jsx file.

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <center>
+        <h1>Meme Generator</h1>
+      </center>
+    </>
+  );
+};
+
+export default HomePage;
