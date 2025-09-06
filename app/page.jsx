dbd1ec2c0131a68e82609e7d579ef98f71abebf8
CoolShivas <shivas2710cool00@gmail.com>
2025-09-06 20:42:21 +0530

    Making the main page.jsx to client component and using the useEffect hook and rest of the react hooks.

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 
 const HomePage = () => {
+  useEffect(() => {
+    const fetchApi = async () => {
+      const response = await fetch("https://api.imgflip.com/get_memes");
+      const result = await response.json();
+      console.log(result); // // Getting the response in the Browser Console;
+      // // // {success: true, data: {…}}
+      // // // data: {memes: Array(100)}
+      // // // success: true
+      // // // [[Prototype]]: Object
+    };
+    fetchApi();
+  }, []);
+
   return (
     <>
       <center>
